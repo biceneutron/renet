c5181bbd06c97bcc8b269ae8ebe1fd6fb35cadfd
biceneutron <biceneutron@gmail.com>
2023-08-08 19:17:57 +0800

    remove all the HTTP stuffs from echo example

--- renet/examples/echo_webrtc.rs
+++ renet/examples/echo_webrtc.rs
@@ -17,42 +17,10 @@ use renet::{
     ConnectionConfig, DefaultChannel, RenetClient, RenetServer, ServerEvent,
 };
 
+use base64::{prelude::BASE64_STANDARD, Engine};
 use rand::Rng;
-use reqwest::{Client as HttpClient, Response as HttpResponse};
-use rouille::Server;
-use rouille::{Request, Response};
-use serde::{Deserialize, Serialize};
-use std::io::Read;
 use str0m::change::{SdpAnswer, SdpOffer};
 use str0m::{Candidate, Rtc};
-
-const SERVER_URL: &str = "http://127.0.0.1:8080";
-
-// Helper struct to pass an username in the user data
-struct Username(String);
-
-impl Username {
-    fn to_netcode_user_data(&self) -> [u8; NETCODE_USER_DATA_BYTES] {
-        let mut user_data = [0u8; NETCODE_USER_DATA_BYTES];
-        if self.0.len() > NETCODE_USER_DATA_BYTES - 8 {
-            panic!("Username is too big");
-        }
-        user_data[0..8].copy_from_slice(&(self.0.len() as u64).to_le_bytes());
-        user_data[8..self.0.len() + 8].copy_from_slice(self.0.as_bytes());
-
-        user_data
-    }
-
-    fn from_user_data(user_data: &[u8; NETCODE_USER_DATA_BYTES]) -> Self {
-        let mut buffer = [0u8; 8];
-        buffer.copy_from_slice(&user_data[0..8]);
-        let mut len = u64::from_le_bytes(buffer) as usize;
-        len = len.min(NETCODE_USER_DATA_BYTES - 8);
-        let data = user_data[8..len + 8].to_vec();
-        let username = String::from_utf8(data).unwrap();
-        Self(username)
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -61,14 +29,14 @@ async fn main() {
 
     let exec_type = &args[1];
     match exec_type.as_str() {
+        "server" => {
+            let server_addr: SocketAddr = format!("127.0.0.1:{}", args[2]).parse().unwrap();
+            server(server_addr);
+        }
         "client" => {
             let server_addr: SocketAddr = format!("127.0.0.1:{}", args[2]).parse().unwrap();
             let username = Username(args[3].clone());
             client(server_addr, username).await;
-        }
-        "server" => {
-            let server_addr: SocketAddr = format!("127.0.0.1:{}", args[2]).parse().unwrap();
-            server(server_addr);
         }
         _ => {
             println!("Invalid argument, first one must be \"client\" or \"server\".");
@@ -77,6 +45,127 @@ async fn main() {
 }
 
 const PROTOCOL_ID: u64 = 7;
+
+fn server(public_addr: SocketAddr) {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let connection_config = ConnectionConfig::default();
+    let mut server: RenetServer = RenetServer::new(connection_config);
+
+    let socket: UdpSocket = UdpSocket::bind(public_addr).unwrap();
+    let addr = socket.local_addr().expect("a local socket adddress");
+    let server_config = ServerConfig {
+        max_clients: 64,
+        protocol_id: PROTOCOL_ID,
+        public_addr,
+        authentication: ServerAuthentication::Unsecure,
+    };
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    let mut transport = NetcodeServerTransport::new(current_time, server_config, socket).unwrap();
+
+    let mut usernames: HashMap<u64, String> = HashMap::new();
+    let mut received_messages = vec![];
+    let mut last_updated = Instant::now();
+
+    let (tx, rx) = mpsc::sync_channel(1);
+
+    // handle terminal input
+    thread::spawn(move || {
+        loop {
+            // wait for answer SDP to be pasted
+            let mut offer_intput = String::new();
+            if let Err(e) = std::io::stdin().read_line(&mut offer_intput) {
+                panic!("Failed reading SDP from terminal: {}", e);
+            };
+            offer_intput = offer_intput.trim().to_owned();
+            let offer = String::from_utf8(decode(offer_intput).expect("Stdin input should be decoded"))
+                .expect("Decoded offer should be converted into String");
+
+            create_server_rtc(offer, addr, tx.clone());
+        }
+    });
+
+    loop {
+        let now = Instant::now();
+        let duration = now - last_updated;
+        last_updated = now;
+
+        transport.spawn_new_client(&rx);
+
+        server.update(duration);
+        transport.update(duration, &mut server).unwrap();
+
+        received_messages.clear();
+
+        while let Some(event) = server.get_event() {
+            match event {
+                ServerEvent::ClientConnected { client_id } => {
+                    let user_data = transport.user_data(client_id).unwrap();
+                    let username = Username::from_user_data(&user_data);
+                    usernames.insert(client_id, username.0);
+                    log::info!("Client {} connected.", client_id)
+                }
+                ServerEvent::ClientDisconnected { client_id, reason } => {
+                    log::info!("Client {} disconnected: {}", client_id, reason);
+                    usernames.remove_entry(&client_id);
+                }
+            }
+        }
+
+        // custom logic
+        for client_id in server.clients_id() {
+            while let Some(message) = server.receive_message(client_id, DefaultChannel::Unreliable) {
+                let text = String::from_utf8(message.into()).unwrap();
+                let username = usernames.get(&client_id).unwrap();
+                log::info!("Client {} ({}) sent text: {}", username, client_id, text);
+
+                let text = format!("{}: {}", username, text);
+                received_messages.push(text);
+            }
+        }
+
+        for text in received_messages.iter() {
+            server.broadcast_message(DefaultChannel::Unreliable, text.as_bytes().to_vec());
+        }
+
+        transport.send_packets(&mut server);
+        thread::sleep(Duration::from_millis(50));
+    }
+}
+
+fn create_server_rtc(offer: String, addr: SocketAddr, tx: SyncSender<(u64, Rtc)>) {
+    static ID_COUNTER: AtomicU64 = AtomicU64::new(0);
+    let client_id = ID_COUNTER.fetch_add(1, Ordering::SeqCst);
+
+    let offer = match SdpOffer::from_sdp_string(&offer) {
+        Ok(sdp) => sdp,
+        Err(e) => {
+            log::error!("Failed to parse SDP offer: {}", e);
+            return;
+        }
+    };
+
+    let mut rtc = Rtc::builder()
+        // Uncomment this to see statistics
+        // .set_stats_interval(Some(Duration::from_secs(1)))
+        // .set_ice_lite(true)
+        .build();
+
+    // Add the shared UDP socket as a host candidate
+    let candidate = Candidate::host(addr).expect("a host candidate");
+    rtc.add_local_candidate(candidate);
+
+    // Create an SDP Answer.
+    let answer = rtc.sdp_api().accept_offer(offer).expect("offer to be accepted");
+
+    println!("\nPaste this SDP to the client terminal:");
+    println!("{}\n", BASE64_STANDARD.encode(answer.to_sdp_string()));
+    println!("\nPaste this client id to the client terminal:");
+    println!("{}\n", client_id);
+
+    // The Rtc instance is shipped off to the main run loop.
+    tx.send((client_id, rtc)).expect("to send Rtc instance");
+}
 
 async fn client(server_addr: SocketAddr, username: Username) {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
@@ -87,7 +176,7 @@ async fn client(server_addr: SocketAddr, username: Username) {
     let socket = UdpSocket::bind(udp_socket_addr).unwrap();
 
     // webrtc
-    let (rtc, client_id) = create_rtc(udp_socket_addr).await;
+    let (rtc, client_id) = create_client_rtc(udp_socket_addr).await;
 
     // renet
     let authentication = ClientAuthentication::Unsecure {
@@ -143,88 +232,7 @@ async fn client(server_addr: SocketAddr, username: Username) {
     log::info!("Str0m is closed");
 }
 
-fn server(public_addr: SocketAddr) {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
-    let connection_config = ConnectionConfig::default();
-    let mut server: RenetServer = RenetServer::new(connection_config);
-
-    let socket: UdpSocket = UdpSocket::bind(public_addr).unwrap();
-    let addr = socket.local_addr().expect("a local socket adddress");
-    let server_config = ServerConfig {
-        max_clients: 64,
-        protocol_id: PROTOCOL_ID,
-        public_addr,
-        authentication: ServerAuthentication::Unsecure,
-    };
-    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
-    let mut transport = NetcodeServerTransport::new(current_time, server_config, socket).unwrap();
-
-    let mut usernames: HashMap<u64, String> = HashMap::new();
-    let mut received_messages = vec![];
-    let mut last_updated = Instant::now();
-
-    let (tx, rx) = mpsc::sync_channel(1);
-
-    // HTTP server
-    thread::spawn(move || {
-        let server = Server::new("127.0.0.1:8080", move |request| web_request(request, addr, tx.clone())).expect("starting the web server");
-
-        let port = server.server_addr().port();
-        log::info!("Connect a browser to https://{:?}:{:?}", addr.ip(), port);
-
-        server.run();
-    });
-
-    loop {
-        let now = Instant::now();
-        let duration = now - last_updated;
-        last_updated = now;
-
-        transport.spawn_new_client(&rx);
-
-        server.update(duration);
-        transport.update(duration, &mut server).unwrap();
-
-        received_messages.clear();
-
-        while let Some(event) = server.get_event() {
-            match event {
-                ServerEvent::ClientConnected { client_id } => {
-                    let user_data = transport.user_data(client_id).unwrap();
-                    let username = Username::from_user_data(&user_data);
-                    usernames.insert(client_id, username.0);
-                    log::info!("Client {} connected.", client_id)
-                }
-                ServerEvent::ClientDisconnected { client_id, reason } => {
-                    log::info!("Client {} disconnected: {}", client_id, reason);
-                    usernames.remove_entry(&client_id);
-                }
-            }
-        }
-
-        // custom logic
-        for client_id in server.clients_id() {
-            while let Some(message) = server.receive_message(client_id, DefaultChannel::Unreliable) {
-                let text = String::from_utf8(message.into()).unwrap();
-                let username = usernames.get(&client_id).unwrap();
-                log::info!("Client {} ({}) sent text: {}", username, client_id, text);
-
-                let text = format!("{}: {}", username, text);
-                received_messages.push(text);
-            }
-        }
-
-        for text in received_messages.iter() {
-            server.broadcast_message(DefaultChannel::Unreliable, text.as_bytes().to_vec());
-        }
-
-        transport.send_packets(&mut server);
-        thread::sleep(Duration::from_millis(50));
-    }
-}
-
-async fn create_rtc(local_addr: SocketAddr) -> (Rtc, u64) {
+async fn create_client_rtc(local_addr: SocketAddr) -> (Rtc, u64) {
     let mut rtc = Rtc::new();
     let local_candidate = match Candidate::host(local_addr) {
         Ok(c) => c,
@@ -235,28 +243,30 @@ async fn create_rtc(local_addr: SocketAddr) -> (Rtc, u64) {
     let mut api = rtc.sdp_api();
     let _ = api.add_channel("data".to_string());
     let (offer, pending) = api.apply().unwrap();
-    let offer_string = offer.to_sdp_string();
 
-    let http_client = HttpClient::new();
-    let response: HttpResponse = loop {
-        let request = http_client
-            .post(SERVER_URL)
-            .header("Content-Length", offer_string.len())
-            .body(offer_string.clone());
+    println!("\nPaste this SDP to the server terminal:");
+    println!("{}\n", BASE64_STANDARD.encode(offer.to_sdp_string()));
 
-        match request.send().await {
-            Ok(resp) => {
-                break resp;
-            }
-            Err(e) => {
-                log::warn!("Failed sending HTTP request: {:?}", e);
-                thread::sleep(Duration::from_millis(1000));
-            }
-        };
+    // wait for answer SDP to be pasted
+    let mut answer_intput = String::new();
+    if let Err(e) = std::io::stdin().read_line(&mut answer_intput) {
+        panic!("Failed reading SDP from terminal: {}", e);
     };
-    let sig_res: SignalingResponse = serde_json::from_str(&response.text().await.unwrap()).unwrap();
+    answer_intput = answer_intput.trim().to_owned();
+    let answer_string = String::from_utf8(decode(answer_intput).expect("Stdin input should be decoded"))
+        .expect("Decoded answer should be converted into String");
 
-    let answer = match SdpAnswer::from_sdp_string(&sig_res.sdp) {
+    // wait for client id to be pasted
+    let mut client_id_intput = String::new();
+    if let Err(e) = std::io::stdin().read_line(&mut client_id_intput) {
+        panic!("Failed reading SDP from terminal: {}", e);
+    };
+    let client_id = match client_id_intput.trim().to_owned().parse::<u64>() {
+        Ok(id) => id,
+        Err(e) => panic!("Failed parsing id from stdin to u64: {}", e),
+    };
+
+    let answer = match SdpAnswer::from_sdp_string(&answer_string) {
         Ok(a) => a,
         Err(e) => panic!("Str0m failed parsing answer SDP: {}", e),
     };
@@ -264,69 +274,7 @@ async fn create_rtc(local_addr: SocketAddr) -> (Rtc, u64) {
         panic!("Str0m failed accepting answer SDP: {}", e);
     };
 
-    (rtc, sig_res.client_id)
-}
-
-fn web_request(request: &Request, addr: SocketAddr, tx: SyncSender<(u64, Rtc)>) -> Response {
-    log::info!("HTTP server got one request");
-
-    static ID_COUNTER: AtomicU64 = AtomicU64::new(0);
-    let client_id = ID_COUNTER.fetch_add(1, Ordering::SeqCst);
-
-    if request.method() == "GET" {
-        return Response::empty_204();
-    }
-
-    // Expected POST SDP Offers.
-    let mut data = request.data().expect("body to be available");
-
-    let mut req_data = String::new();
-    if let Some(e) = data.read_to_string(&mut req_data).err() {
-        panic!("error parsing HTTP request {}", e);
-    }
-
-    let offer = match SdpOffer::from_sdp_string(&req_data) {
-        Ok(sdp) => sdp,
-        Err(e) => {
-            log::error!("Failed to parse SDP offer: {}", e);
-            return Response::empty_400();
-        }
-    };
-
-    let mut rtc = Rtc::builder()
-        // Uncomment this to see statistics
-        // .set_stats_interval(Some(Duration::from_secs(1)))
-        // .set_ice_lite(true)
-        .build();
-
-    // Add the shared UDP socket as a host candidate
-    let candidate = Candidate::host(addr).expect("a host candidate");
-    rtc.add_local_candidate(candidate);
-
-    // Create an SDP Answer.
-    let answer = rtc.sdp_api().accept_offer(offer).expect("offer to be accepted");
-
-    // The Rtc instance is shipped off to the main run loop.
-    tx.send((client_id, rtc)).expect("to send Rtc instance");
-
-    let response = match serde_json::to_string(&SignalingResponse::new(answer.to_sdp_string(), client_id)) {
-        Ok(res) => res,
-        Err(e) => return Response::text(format!("Server failed creating answer SDP: {}", e)).with_status_code(500),
-    };
-
-    Response::text(response)
-}
-
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-pub struct SignalingResponse {
-    pub sdp: String,
-    pub client_id: u64,
-}
-
-impl SignalingResponse {
-    pub fn new(sdp: String, client_id: u64) -> SignalingResponse {
-        SignalingResponse { sdp, client_id }
-    }
+    (rtc, client_id)
 }
 
 fn spawn_stdin_channel() -> Receiver<String> {
@@ -337,4 +285,35 @@ fn spawn_stdin_channel() -> Receiver<String> {
         tx.send(buffer.trim_end().to_string()).unwrap();
     });
     rx
+}
+
+fn decode(encoded: String) -> Result<Vec<u8>, base64::DecodeError> {
+    let decoded = BASE64_STANDARD.decode(encoded)?;
+    Ok(decoded)
+}
+
+// Helper struct to pass an username in the user data
+struct Username(String);
+
+impl Username {
+    fn to_netcode_user_data(&self) -> [u8; NETCODE_USER_DATA_BYTES] {
+        let mut user_data = [0u8; NETCODE_USER_DATA_BYTES];
+        if self.0.len() > NETCODE_USER_DATA_BYTES - 8 {
+            panic!("Username is too big");
+        }
+        user_data[0..8].copy_from_slice(&(self.0.len() as u64).to_le_bytes());
+        user_data[8..self.0.len() + 8].copy_from_slice(self.0.as_bytes());
+
+        user_data
+    }
+
+    fn from_user_data(user_data: &[u8; NETCODE_USER_DATA_BYTES]) -> Self {
+        let mut buffer = [0u8; 8];
+        buffer.copy_from_slice(&user_data[0..8]);
+        let mut len = u64::from_le_bytes(buffer) as usize;
+        len = len.min(NETCODE_USER_DATA_BYTES - 8);
+        let data = user_data[8..len + 8].to_vec();
+        let username = String::from_utf8(data).unwrap();
+        Self(username)
+    }
 }
